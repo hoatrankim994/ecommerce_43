87738fff36793729114cfda151b9b8f0ce5acb27
TranKimHoa <tran.kim.hoa@framgia.com>
2018-05-14 17:11:12 +0700

    manage_list_categories

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,6 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many :order_details, dependent: :destroy
   has_many :orders, through: :order_detail
-  mount_uploader :image, PictureUploader
   validates :productname, presence: true
   validates :productcontent, presence: true
   validate  :image_size
@@ -11,9 +10,12 @@ class Product < ApplicationRecord
   validates :onhand, numericality: true
   validates :status, presence: true
   validates :author, presence: true
-  enum status: {hide: 0, show: 1}
   scope :order_by_created, ->{order(created_at: :desc)}
   scope :by_status, ->(status){where(status: status)}
+  enum status: {hide: 0, show: 1}
+  delegate :title, to: :category, prefix: true, allow_nil: true
+  mount_uploader :image, PictureUploader
+
   def image_size
     return if image.size <= Settings.pic_size.megabytes
     errors.add(:image, t("model.user.pic_less_5"))
