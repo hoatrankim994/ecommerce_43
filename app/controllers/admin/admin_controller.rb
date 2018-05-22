3de26ba6679b17b0bc8c263824f19d2c1d38f1cd
TranKimHoa <tran.kim.hoa@framgia.com>
2018-05-22 17:47:38 +0700

    template_admin

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,7 +1,12 @@
 class Admin::AdminController < ApplicationController
+  layout "admin/application"
+  before_action :require_admin
+
   def require_admin
     return if current_user && current_user.admin?
     flash[:danger] = t "controller.orders.role"
-    redirect_to root_url
+    redirect_to login_url
   end
+
+  def index; end
 end
